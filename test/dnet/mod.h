c8fe9086c497b480576fe6b186172e227319b963
dugsong <dugsong>
2002-03-29 06:07:27 +0000

    simpler

--- test/dnet/mod.h
+++ test/dnet/mod.h
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 2002 Dug Song <dugsong@monkey.org>
  *
- * $Id: mod.h,v 1.1 2002/02/08 07:26:58 dugsong Exp $
+ * $Id: mod.h,v 1.2 2002/03/29 06:07:27 dugsong Exp $
  */
 
 #ifndef MOD_H
@@ -21,6 +21,6 @@ struct mod {
 #define MOD_TYPE_DATA	0x01	/* generate data */
 #define MOD_TYPE_ENCAP	0x02	/* encapsulate data */
 #define MOD_TYPE_XMIT	0x04	/* send datagrams */
-#define MOD_TYPE_KERN	0x08	/* manipulate kernel networking information */
+#define MOD_TYPE_KERN	0x08	/* kernel network info */
 
 #endif /* MOD_H */
