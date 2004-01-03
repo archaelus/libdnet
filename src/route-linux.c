97892fc6050c5c7889a9e34132a145c04bfc7f84
dugsong <dugsong>
2004-01-03 17:27:18 +0000

    simplify with ADDR_ISHOST() macro

--- src/route-linux.c
+++ src/route-linux.c
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 2000 Dug Song <dugsong@monkey.org>
  *
- * $Id: route-linux.c,v 1.12 2002/02/08 07:41:04 dugsong Exp $
+ * $Id: route-linux.c,v 1.13 2004/01/03 17:27:18 dugsong Exp $
  */
 
 #include "config.h"
@@ -27,6 +27,11 @@
 #include <unistd.h>
 
 #include "dnet.h"
+
+#define ADDR_ISHOST(a)	(((a)->addr_type == ADDR_TYPE_IP &&	\
+			  (a)->addr_bits == IP_ADDR_BITS) ||	\
+			 ((a)->addr_type == ADDR_TYPE_IP6 &&	\
+			  (a)->addr_bits == IP6_ADDR_BITS))
 
 #define PROC_ROUTE_FILE	"/proc/net/route"
 
@@ -63,21 +68,22 @@ int
 route_add(route_t *r, const struct route_entry *entry)
 {
 	struct rtentry rt;
+	struct addr dst;
 
 	memset(&rt, 0, sizeof(rt));
+	rt.rt_flags = RTF_UP | RTF_GATEWAY;
 
-	if (addr_ntos(&entry->route_dst, &rt.rt_dst) < 0 ||
-	    addr_ntos(&entry->route_gw, &rt.rt_gateway) < 0)
+	if (ADDR_ISHOST(&entry->route_dst)) {
+		rt.rt_flags |= RTF_HOST;
+		memcpy(&dst, &entry->route_dst, sizeof(dst));
+	} else
+		addr_net(&entry->route_dst, &dst);
+	
+	if (addr_ntos(&dst, &rt.rt_dst) < 0 ||
+	    addr_ntos(&entry->route_gw, &rt.rt_gateway) < 0 ||
+	    addr_btos(entry->route_dst.addr_bits, &rt.rt_genmask) < 0)
 		return (-1);
-
-	if (entry->route_dst.addr_bits < IP_ADDR_BITS) {
-		rt.rt_flags = RTF_UP | RTF_GATEWAY;
-		if (addr_btos(entry->route_dst.addr_bits, &rt.rt_genmask) < 0)
-			return (-1);
-	} else {
-		rt.rt_flags = RTF_UP | RTF_HOST | RTF_GATEWAY;
-		addr_btos(IP_ADDR_BITS, &rt.rt_genmask);
-	}
+	
 	return (ioctl(r->fd, SIOCADDRT, &rt));
 }
 
@@ -85,20 +91,21 @@ int
 route_delete(route_t *r, const struct route_entry *entry)
 {
 	struct rtentry rt;
-
+	struct addr dst;
+	
 	memset(&rt, 0, sizeof(rt));
+	rt.rt_flags = RTF_UP;
 
-	if (addr_ntos(&entry->route_dst, &rt.rt_dst) < 0)
+	if (ADDR_ISHOST(&entry->route_dst)) {
+		rt.rt_flags |= RTF_HOST;
+		memcpy(&dst, &entry->route_dst, sizeof(dst));
+	} else
+		addr_net(&entry->route_dst, &dst);
+	
+	if (addr_ntos(&dst, &rt.rt_dst) < 0 ||
+	    addr_btos(entry->route_dst.addr_bits, &rt.rt_genmask) < 0)
 		return (-1);
-
-	if (entry->route_dst.addr_bits < IP_ADDR_BITS) {
-		rt.rt_flags = RTF_UP;
-		if (addr_btos(entry->route_dst.addr_bits, &rt.rt_genmask) < 0)
-			return (-1);
-	} else {
-		rt.rt_flags = RTF_UP | RTF_HOST;
-		addr_btos(IP_ADDR_BITS, &rt.rt_genmask);
-	}
+	
 	return (ioctl(r->fd, SIOCDELRT, &rt));
 }
 
